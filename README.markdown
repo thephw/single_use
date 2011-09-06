5d866a4556449a9eab2d45c5b03b1ca0080d5417
Patrick Howard Wiseman <patrick.howard.wiseman@gmail.com>
2011-09-06 12:44:39 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,2 @@
+# Single Use Project #
+This is a web application survey and results manager to help the Emory medical team with their study for the [*ASA conference*](http://www.asahq.org/Annual-Meeting.aspx)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SingleUse::Application.config.secret_token = 'd40f7181b2f7fa02d722090abcc663bbf30cd606adcc7c948d0feebd81b0b13fa51b2d44dd1894629ce1a99521db3c7faaed7615af76247293b63bd2c4386d80'
