99befc670684df1b8f04c6361c553a9170d6391f
Patrick Howard Wiseman <patrick.howard.wiseman@gmail.com>
2011-09-08 17:01:09 -0400

    Stubbed out a bunch more resources

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source 'http://rubygems.org'
+
 gem 'rails', '3.0.10'
 gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 gem 'heroku'
+gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'googlecharts'
 
 group :development  do
   gem 'rspec', '2.6.0'
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,8 @@
 module PagesHelper
+  def gravatar_for(email, options = { :size => 50 } )
+    gravatar_image_tag(email.downcase,
+                      :alt => email,
+                      :class => 'gravatar',
+                      :gravatar => options)
+  end
 end
