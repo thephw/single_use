da2e4a719bc0753196260b9cbb3415b0d8937602
Patrick Howard Wiseman <patrick.howard.wiseman@gmail.com>
2011-09-15 12:07:11 -0400

    Updated form for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   
   serialize :answers, Hash
 
+  validates :all, :presence => true
+  
   #def validate
   #  Question.all.each do |q|
   #    errors.add_to_base "Have to answer question #{q.id}" if self["q#{q.id}"].nil?
