ee138e1e376bf95d6d9930b46cd203f49823ced6
Patrick Howard Wiseman <patrick.howard.wiseman@gmail.com>
2011-09-15 22:51:05 -0400

    Get validation and javascript working with new user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  
+  validate :survey_validator
+
+  def survey_validator
+    Question.all.each do |q|
+      if(self["q#{q.id}"].nil?)
+        errors.add(:base, "Must answer question #{q.id}")
+      elsif (self["q#{q.id}"].empty?)
+        errors.add(:base, "Must answer question #{q.id}")
+      end
+    end
+  end
+
 end
