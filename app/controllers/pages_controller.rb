373f19cf9c98577ee64fdac86867d594ff102ba2
Patrick Howard Wiseman <patrick.howard.wiseman@gmail.com>
2011-09-15 20:41:50 -0400

    Get basic graphing functionality working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  
+  http_basic_authenticate_with :name => "admin", :password => "singleuse", :except => :home, :except => [:home, :about, :survey]
+  
   def home
     @title = 'Home'
   end
@@ -13,6 +16,7 @@ class PagesController < ApplicationController
   end
 
   def results
+    
     @title = 'Survey Results'
   end
 
