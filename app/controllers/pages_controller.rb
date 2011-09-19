a9aea93e72bac7b3baaa69c4ef9a27f726fdbd20
Patrick Howard Wiseman <patrick.howard.wiseman@gmail.com>
2011-09-19 13:52:58 -0400

    Subdivided results to get quicker load times

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,8 +16,30 @@ class PagesController < ApplicationController
   end
 
   def results
-    
     @title = 'Survey Results'
   end
 
+  def overall_results
+    @title = 'Overall Results'
+  end
+  
+  def results_by_age 
+    @title = 'Results by Age'
+  end
+  
+  def results_by_setting
+    @title = 'Results by Setting'
+  end
+  
+  def results_by_gender
+    @title = 'Results by Gender'
+  end
+  
+  def results_by_experience
+    @title = 'Resutls by Experience'
+  end
+  
+  def results_by_region
+     @title = 'Results by Region'
+  end
 end
